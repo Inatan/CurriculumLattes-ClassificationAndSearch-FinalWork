71aa07754957cf06296a9d2156f51b2ab1ec4ce0
Inatan Lopes Hertzog <Inatan_hertzog@hotmail.com>
2013-06-15 16:57:28 -0300

    Colocar arquivos para o trabalho
    para sincronizar tudo

---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,71 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //quando selecionar, guarda caminho de cada file em um vetor de strings
+            //para cada caminho, abrir o arquivo XML
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.Load("curriculo.xml");
+            Console.WriteLine(xmldoc.DocumentElement.Name);
+            for (int i = 0; i < xmldoc.DocumentElement.ChildNodes.Count; i++)
+            {
+                //Escreve na tela o conteudo do nó filho
+                if (xmldoc.DocumentElement.ChildNodes[i].InnerText != "")
+                {
+                    Console.WriteLine(xmldoc.DocumentElement.ChildNodes[i].Name/* + "<br>"*/);
+                    XmlNode decida1 = xmldoc.DocumentElement.ChildNodes[i];
+                    for (int j = 0; j < decida1.ChildNodes.Count; j++)
+                    {
+                        if (decida1.ChildNodes[j].InnerText != "")
+                        {
+                            Console.WriteLine(decida1.ChildNodes[j].FirstChild.FirstChild.FirstChild/* + "<br>"*/);
+                        }
+                    }
+                }
+            }
+
+            /*
+            XmlElement root = (XmlElement)xmldoc.DocumentElement.FirstChild;
+            XmlNode next = xmldoc.SelectSingleNode("CURRICULO-VITAE/DADOS-GERAIS");
+            XmlElement nome = (XmlElement)next;
+
+            Console.WriteLine(nome.GetAttribute("NOME-COMPLETO"));*/
+         /*   foreach (XmlNode filho in next)
+            {
+                XmlElement elemento = (XmlElement)filho.NextSibling;
+                if (elemento.HasAttribute("TEXTO-RESUMO-CV-RH"))
+                {
+                    string curso = elemento.GetAttribute("TEXTO-RESUMO-CV-RH");
+                    Console.WriteLine(curso);
+                }
+            }
+
+            //Aqui eu faço um cast para transformar o .FirstChild que devolve um nodo para um elemento,
+            //assim podendo ler os atributos
+            if (root.HasAttribute("NOME-COMPLETO"))
+            {
+                String nome = root.GetAttribute("NOME-COMPLETO"); //WORKS BITCH
+                Console.WriteLine(nome);
+            }*/
+            
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WindowsFormsApplication1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+
+namespace WindowsFormsApplication1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            FolderBrowserDialog folderbrowser = new FolderBrowserDialog();
+            folderbrowser.Description = "Selecione o diretório que contem os arquivos .xml";
+            if (folderbrowser.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                //quando selecionar, guarda caminho de cada file em um vetor de strings
+                string[] files = Directory.GetFiles(folderbrowser.SelectedPath);
+                foreach (string s in files)
+                {
+                    //para cada caminho, abrir o arquivo XML
+                    XmlDocument xmldoc = new XmlDocument();
+                    xmldoc.Load(s);
+
+                    XmlElement root = (XmlElement)xmldoc.DocumentElement.FirstChild;
+                    XmlNodeList next = xmldoc.DocumentElement.ChildNodes;
+
+                    foreach (XmlNode filho in next)
+                    {
+                        XmlElement elemento = (XmlElement)filho.FirstChild.FirstChild;
+                        if (elemento.HasAttribute("BAIRRO"))
+                        {
+                            string curso = elemento.GetAttribute("BAIRRO");
+                            MessageBox.Show(curso);
+                        }
+                    }
+
+                    //Aqui eu faço um cast para transformar o .FirstChild que devolve um nodo para um elemento,
+                    //assim podendo ler os atributos
+
+                    if (root.HasAttribute("NOME-COMPLETO"))
+                    {
+                        String nome = root.GetAttribute("NOME-COMPLETO"); //WORKS BITCH
+                        MessageBox.Show(nome);
+                    }
+
+
+
+
+
+                }
+
+            }
+        }
+
+    }
+}
